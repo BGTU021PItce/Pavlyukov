5144466831859bce283417124d0744aaef6bbc47
LordXalgoz <in5pekt0r@yandex.ru>
2021-12-10 15:31:47 +0300

    my tasks

---
+++ 4_1.py
@@ -0,0 +1,3 @@
+a = int(input("Введите a: "))
+b = int(input("Введите b: "))
+print(int((a+b)/2))
---
+++ 4_2.py
@@ -0,0 +1,3 @@
+a = int(input("Введите a: "))
+b = int(input("Введите b: "))
+print(int((b-a-1)/2))
